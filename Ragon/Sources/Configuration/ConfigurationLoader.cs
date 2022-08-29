72ff37e94a667aac768731a82754723dbd81c08f
Edmand46 <theedison4@gmail.com>
2022-08-30 01:54:50 +0400

    bump version

--- Ragon/Sources/Configuration/ConfigurationLoader.cs
+++ Ragon/Sources/Configuration/ConfigurationLoader.cs
@@ -9,7 +9,7 @@ namespace Ragon.Core
   public static class ConfigurationLoader
   {
     private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
-    private static readonly string _serverVersion = "1.0.16-rc";
+    private static readonly string _serverVersion = "1.0.17-rc";
 
     private static void CopyrightInfo()
     {
