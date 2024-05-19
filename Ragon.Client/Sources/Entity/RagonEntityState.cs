3bec19c2b21d3788127c40e0a86ff039048cdf49
edmand46 <theedison4@gmail.com>
2024-05-19 12:26:42 +0300

    chore: updated copyright

--- Ragon.Client/Sources/Entity/RagonEntityState.cs
+++ Ragon.Client/Sources/Entity/RagonEntityState.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2023 Eduard Kargin <kargin.eduard@gmail.com>
+ * Copyright 2023-2024 Eduard Kargin <kargin.eduard@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
